b436ca707d296af3b5670360def1d677dc226279
Benjamin <bmarquiscom@gmail.com>
2020-10-25 14:11:36 -0400

    Add read Only

--- src/general-utils/object.ts
+++ src/general-utils/object.ts
@@ -8,3 +8,13 @@ export const sanitizeObject = curryN(
   <M extends Record<string, any>>(keysToSanitize: string[], object: M) =>
     omit(keysToSanitize, object)
 );
+
+export const readOnly = <M = Record<string, unknown>>(
+  object: M
+): Readonly<M> => {
+  Object.values(object).forEach((value) => {
+    if (value && typeof value === "object") readOnly(value);
+  });
+
+  return Object.freeze(object);
+};
