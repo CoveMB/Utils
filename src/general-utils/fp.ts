e5c2e2209566b0fe1a19cf486a97ab32721cc4bd
Benjamin <bmarquiscom@gmail.com>
2020-11-28 16:53:37 -0500

    Add try catch async

--- src/general-utils/fp.ts
+++ src/general-utils/fp.ts
@@ -1,5 +1,16 @@
+import * as R from "ramda";
+
 export const debugLog = <M>(value: M): M => {
   console.log(value);
 
   return value;
 };
+
+export const tryCatchAsync = R.curry(async (_fn, _onError, _object) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/return-await
+    return await _fn(_object);
+  } catch (error) {
+    return _onError(error, _object);
+  }
+});
