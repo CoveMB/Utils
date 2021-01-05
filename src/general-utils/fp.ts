2a6bc57becdee697894b6c53364ff7c8ce7d7faa
Benjamin <bmarquiscom@gmail.com>
2021-01-04 20:57:32 -0500

    Add pipe async

--- src/general-utils/fp.ts
+++ src/general-utils/fp.ts
@@ -14,3 +14,9 @@ export const tryCatchAsync = R.curry(async (_fn, _onError, _object) => {
     return _onError(error, _object);
   }
 });
+
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+export const pipeAsync = <T, Y = unknown>(...fns: any[]) => (
+  input?: Y
+): Promise<T> =>
+  fns.reduce((promise, fn) => promise.then(fn), Promise.resolve(input));
